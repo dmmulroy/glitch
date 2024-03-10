92d828d56a5a8c205d0091e753f21d8304364eff
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-03-09 22:09:05 -0500

    batman

---
+++ README.md
@@ -0,0 +1,25 @@
+# glitch
+
+[![Package Version](https://img.shields.io/hexpm/v/glitch)](https://hex.pm/packages/glitch)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-ffaff3)](https://hexdocs.pm/glitch/)
+
+```sh
+gleam add glitch
+```
+```gleam
+import glitch
+
+pub fn main() {
+  // TODO: An example of the project in use
+}
+```
+
+Further documentation can be found at <https://hexdocs.pm/glitch>.
+
+## Development
+
+```sh
+gleam run   # Run the project
+gleam test  # Run the tests
+gleam shell # Run an Erlang shell
+```
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs_master.url = "github:NixOS/nixpkgs?ref=master";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: 
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with inputs'.nixpkgs_master.legacyPackages; [gleam erlang_26];
+          };
+        };
+      };
+      flake = {};
+    };
+}
